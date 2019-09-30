ebd6803354856614b6d2e35e6514da1354206544
Hooman Malekzadeh <Hooman.malekzadeh@outlook.com>
2019-09-30 15:04:53 +0100

    ThefirstDayCommit

--- TheFirstDay/TheFirstDay/TheFirstDay.cpp
+++ TheFirstDay/TheFirstDay/TheFirstDay.cpp
@@ -1,14 +1,20 @@
 // TheFirstDay.cpp : Defines the entry point for the console application.
 //
-
+#include "stdafx.h"
 #include <iostream>
 
 using namespace std;
 int main()
 {
+	int a;
+	cout << "hello" << "\n" << "there" << "\n";
+	a = 1;
 
-	cout << "hello" << "\n" << "there";
-	
+	string b = "HellogRANDMA";
+
+	a = 2;
+	cout << "Happens here";
+
     return 0;
 }
 
