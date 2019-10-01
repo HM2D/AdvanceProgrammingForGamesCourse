b20ae77946d46ba4203a04c76190afd6133f300c
Hooman Malekzadeh <Hooman.malekzadeh@outlook.com>
2019-10-01 11:38:22 +0100

    Update TheFirstDay.cpp

--- TheFirstDay/TheFirstDay/TheFirstDay.cpp
+++ TheFirstDay/TheFirstDay/TheFirstDay.cpp
@@ -1,20 +1,20 @@
 // TheFirstDay.cpp : Defines the entry point for the console application.
-//
+// Hooman Malekzadeh b9055911 01/10/2019
 #include "stdafx.h"
 #include <iostream>
 
 using namespace std;
-int main()
+void main()
 {
 	int a;
 	cout << "hello" << "\n" << "there" << "\n";
 	a = 1;
-
 	string b = "HellogRANDMA";
-
 	a = 2;
-	cout << "Happens here";
+	cout << "Happens here";  
 
-    return 0;
+
+
+
 }
 
